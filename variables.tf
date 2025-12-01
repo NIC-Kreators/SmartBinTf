6e42b402042a326e790d409d1c0dd0482edd1bab
Andrey Kirik <dev.andrey.kirik@gmail.com>
2025-12-01 20:39:23 +0500

    ci: update tf version to 1.14.0

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# variable "aws_region" {
+#   type = string
+#   default = "eu-central-1"
+#   description = "AWS region to deploy resources in"
+# }
+
+# variable "project_name" {
+#   type = string
+#   description = "The project name"
+# }
+
+# variable "environment" {
+#   type = string
+#   description = "The current project's environemnt"
+# }
