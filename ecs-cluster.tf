865b8c2fbfd8437047f52ccaa5f41d709b3c357c
Andrey Kirik <dev.andrey.kirik@gmail.com>
2025-12-23 03:43:00 +0500

    fix: ggagaggagaggagagagaggaga

--- ecs-cluster.tf
+++ ecs-cluster.tf
@@ -35,7 +35,7 @@ resource "aws_ecs_capacity_provider" "main" {
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.ecs.arn
-    managed_termination_protection = "ENABLED"
+    managed_termination_protection = "DISABLED"
 
     managed_scaling {
       maximum_scaling_step_size = 1000
