d9f598426a6b1f2954d74a482d05c50dfdc37dcf
Andrey Kirik <dev.andrey.kirik@gmail.com>
2025-12-23 05:38:20 +0500

    fix: add sg for mq

--- mqtt-broker.tf
+++ mqtt-broker.tf
@@ -1,3 +1,52 @@
+resource "aws_security_group" "mqtt_broker_sg" {
+  name        = "${var.project_name}-mqtt-sg"
+  description = "Security group for RabbitMQ MQTT broker"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "MQTT"
+    from_port   = 1883
+    to_port     = 1883
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "MQTTS"
+    from_port   = 8883
+    to_port     = 8883
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+
+  ingress {
+    description = "AMQPS"
+    from_port   = 5671
+    to_port     = 5671
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "RabbitMQ Web Console"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "smart-bin-mqtt-sg"
+  }
+}
+
 resource "aws_mq_broker" "mqtt_broker" {
   broker_name        = "${lower(var.project_name)}-mqtt"
   engine_type        = "RabbitMQ"
@@ -7,6 +56,7 @@ resource "aws_mq_broker" "mqtt_broker" {
 
   subnet_ids          = [aws_subnet.public[0].id]
   publicly_accessible = true
+  security_groups = [ aws_security_group.mqtt_broker_sg.id ]
 
   configuration {
     id       = aws_mq_configuration.rabbitmq_config.id
